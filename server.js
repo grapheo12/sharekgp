4c0b5569559320768b8eb20f38428fd5de09d707
Shubham <smishra99.iitkgp@gmail.com>
2019-04-29 16:17:19 +0530

    Changed database location

--- server.js
+++ server.js
@@ -1,40 +1,62 @@
 var express = require('express');
 var bp = require('body-parser');
 var urlencodedParser = bp.urlencoded({extended: false});
+const request = require('request');
 
 var fs = require("fs");
 
 var app = express();
-app.use(express.static("public"));
+app.use(express.static(__dirname));
+
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/public/main.html");
+    res.sendFile(__dirname + "/main.html");
 });
 
 app.post('/getreq', urlencodedParser, (req, res) => {
     console.log(`${req.body.day} ${req.body.month} ${req.body.year}`);
     let filename = req.body.day + '_' + req.body.month + '_' + req.body.year + '.json';
-    fs.readFile(__dirname + '/traveldata/' + filename, (err, data) => {
-        if (err)
-            res.send(JSON.stringify({number: 0, reqs:[]}));
-        else
-            res.send(data);
-    });
+    try{
+        request.get("clouder.mywebcommunity.org/traveldata/" + filename, (err, response, body) => {
+            if (err)
+                throw err;
+            console.log(body);
+            res.send(body);
+        });
+    }catch(err){
+        res.send(JSON.stringify({number: 0, reqs: []}));
+    }
 });
 
 app.post('/postreq', urlencodedParser, (req, res) => {
-    fs.readFile(__dirname + '/traveldata/' + req.body.day + '.json', (err, data) => {
-        if (err){
-            fs.writeFile(__dirname + '/traveldata/' + req.body.day + '.json', JSON.stringify({number: 1, reqs:[{name: req.body.name, time: req.body.time, desc: req.body.desc}]}), () => {});
-        }else{
-            let datobj = JSON.parse(data);
+    let filename = "traveldata/" + req.body.day + '.json';
+    try{
+        request.get("clouder.mywebcommunity.org/traveldata/" + filename, (err, res, body) => {
+            if (err)
+                throw err;
+            let datobj = JSON.parse(body);
             datobj.number++;
             datobj.reqs.push({name: req.body.name, time: req.body.time, desc: req.body.desc});
-            fs.writeFile(__dirname + '/traveldata/' + req.body.day + '.json', JSON.stringify(datobj), () => {});
-        }
+            let options = {
+                url: "clouder.mywebcommunity.org/sharetravel.php",
+                form: {
+                    file: filename, 
+                    content: JSON.stringify(datobj)
+                }
+            };
 
-        res.send("OK");
-    });
+            request.post(options, (err, response, body) => {res.send("OK");});
+        });
+    }catch(err){
+        let options = {
+            url: "clouder.mywebcommunity.org/sharetravel.php",
+            form: {
+                file: filename, 
+                content: JSON.stringify({number: 1, reqs:[{name: req.body.name, time: req.body.time, desc: req.body.desc}]})
+            }
+        };
+        request.post(options, (err, response, body) => {res.send("OK");});
+    } 
 });
         
 
